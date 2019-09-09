100da228d38a2a24c07e63b73d60d66dda61e482
KarstHub <andreas.hartmann@hydmod.uni-freiburg.de>
2019-09-09 12:13:43 +0200

    Update download.R

--- download.R
+++ download.R
@@ -10,29 +10,23 @@ sourceModule <- paste0(basePath, 'sourceModules/')
 source(paste0(sourceModule,'fileIO.R'))
 
 # creates output folder
-dataOut <- paste0(basePath,'WoKaS_Dynamic_Datasets')
+dataOut <- paste0(basePath,'data')
 dir.create(dataOut)
 
-packageToInstall <- function(package, repos=NULL){
-  if(is.null(repos)) repos='https://cran.r-project.org/'
-  if(package %in% rownames(installed.packages()) == FALSE) install.packages(package, repos)
-  else cat(sprintf('-> %s already installed\n', package))
-}
-
 # install required packages
-packageToInstall('yaml')
-packageToInstall('XML')
-packageToInstall('httr')
-packageToInstall('pdftools')
-packageToInstall('rgdal')
-packageToInstall('tidyverse')
-packageToInstall('jsonlite')
-packageToInstall('curl')
+fileIO.packageRequire('yaml')
+fileIO.packageRequire('XML')
+fileIO.packageRequire('httr')
+fileIO.packageRequire('rgdal')
+fileIO.packageRequire('jsonlite')
+fileIO.packageRequire('curl')
 if("RHTMLForms" %in% rownames(installed.packages()) == FALSE) {install.packages(paste0(sourceModule,"RHTMLForms_0.6-0.tar.gz"), repos = NULL, type = "source")}
 
 
 # download datasets
 # to download all online dataset, run fileIO.runDowload()
-# to download dataset from specific country, run fileIO.runDownload("country's ISO-2 code")
+# to download dataset from specific country, run fileIO.runDownload("country's name")
+# list of country names download is possible: Germany, France, Austria, Ireland, Slovenia, US, UK
 
-fileIO.runDownload(Germany)
+# download datasets
+fileIO.runDownload()
