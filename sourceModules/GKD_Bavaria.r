c41dfa7a06b1f4666577c3fbb0a0def8174fee58
ayolawale <ayolawale@gmail.com>
2023-03-30 15:44:38 +0200

    I have added the new Bavaria script

--- sourceModules/GKD_Bavaria.r
+++ sourceModules/GKD_Bavaria.r
@@ -1,9 +1,10 @@
+
 # required packages
 library("XML")
 library("rgdal")
 library("httr")
 library("pdftools")
-library("googledrive")
+
 Sys.setenv(LANGUAGE="en")
 
 basePath ='./'
@@ -19,102 +20,147 @@ dir.create(outfolder)
 ##    Download spring discharge datasets    ==
 ##============================================
 # load table data from gwk website
-baseUrl <- "https://www.gkd.bayern.de/de/grundwasser/quellschuettung/tabellen"
+baseUrl <- "https://www.gkd.bayern.de/de/grundwasser/quellschuettung//tabellen"
 
 # springs gauge stations
 wokasRDS <- readRDS(paste0(sourceModule,"/station_info.rds"))
 stationInfo <- subset(wokasRDS, ISO == "DE" & Research_group == "GKD" )
 
-##### Download the dataset from Github repository
-url <-"https://raw.githubusercontent.com/ayolawale/Karst_Project/main/GKD%20Bavaria%20Data-20230309T141844Z-001.zip" 
-download.file(url, dest="./dataset_GKD.zip", mode="wb" ) 
-unzip("dataset_GKD.zip", exdir = "./WoKaS_Dynamic_Datasets/")
-fileNames <- unzip("dataset_GKD.zip", exdir = "./WoKaS_Dynamic_Datasets/")
+# for every karst index
+#for(i in 1:length(karstIndices)){
+for(i in 1:nrow(stationInfo)){  
+  
+	selectedId <- stationInfo$Local_database_ID[i]
+
+	# url needed for the data request
+	dlReqUrl <- "https://www.gkd.bayern.de/de/downloadcenter/enqueue_download" 
 	
+	# url needed for the data download
+	dlBaseUrl <- "https://www.gkd.bayern.de/de/downloadcenter/add_download"
+	content_type = "application/x-www-form-urlencoded; charset=UTF-8"
+	body = list(zr="gesamt",begin="01.07.2018",email="",ende="23.07.2018",wertart="tmw",f="",
+				t=sprintf('{"%s":["grundwasser.quelle"]}',selectedId))
+	
+	# send a http POST (server then starts to prepare the download and returns a "deep link"
+	# in this case the additional header information 'X-Requested-With' = 'XMLHttpRequest' is critical
+	r <- POST(dlReqUrl, content_type = content_type, body = body, encode="form", add_headers('X-Requested-With' = 'XMLHttpRequest'))
+	
+	if(r$status!=200){
+		cat("problem while requesting download :(")
+		next
+	}
+	
+	# extract deep link from the servers response to the POST
+	dlDeeplink <- gsub('"}?','',strsplit(content(r,'text'),'deeplink":')[[1]][2])
+	dlDeeplink2 <- gsub('\\\\','\\', dlDeeplink)
+	
+	# paste the download link together
+	dlUrl <- dlDeeplink2
+
+	# send a http GET until the response page contains a link which can be identified by '>hier</a>'
+	cat("wait for download")
+	for(tryCount in 1:20){
+		cat('.')
+		r <- GET(dlUrl)
+		x <- strsplit(content(r,'text'), "'>hier</a>")[[1]]
+		if(length(x)>1) break
+		Sys.sleep(2)
+	}
+	
+	if(length(x)<2){
+		cat("download timeout :(\n")
+		next
+	}
+	cat("-> download")
+
+	tmpFileArchive <- paste0(outfolder,'/temp.zip')
+	# get the file (url to the file is dlUrl pasted together with '&dl=1')
+	download.file(paste0(dlUrl,'&dl=1'), tmpFileArchive, mode="wb", quiet=F)
+
+	cat(" -> processing")
 ##==========================================================
 ##                      SECTION 2:                        ==  
 ##  Unzip downloaded file, re-format and homogenisation   ==  
 ##==========================================================
-# 	# get the names of files within the archive
-# fileNames <- unzip(tmpFileArchive, list=TRUE)
+	# get the names of files within the archive
+	fileNames <- unzip(tmpFileArchive, list=TRUE)[,'Name']
 	
-# filter theses names and only continue with file names ending with 'csv'
-fileNames <- fileNames[grep('csv$',fileNames)]
+	# filter theses names and only continue with file names ending with 'csv'
+	fileNames <- fileNames[grep('csv$',fileNames)]
 
-# extract id, start and end for each file 
-fileInfos <- NULL
-for(f in fileNames){
-	f_last <- gsub('.*/','',f)
-	x <- unlist(strsplit(f_last, '_'))
-	if(length(x)==3) next
-	id = x[1]
-	if(length(x)==4){
-	  start = as.Date(x[2], "%d.%m.%Y")
-		end = as.Date(x[3], "%d.%m.%Y")
-	}else{
-		start = as.Date(NA)
-		end = as.Date(x[4], "%d.%m.%Y")
+	# extract id, start and end for each file 
+	fileInfos <- NULL
+	for(f in fileNames){
+		f_last <- gsub('.*/','',f)
+		x <- unlist(strsplit(f_last, '_'))
+		if(length(x)==3) next
+		id = x[1]
+		if(length(x)==4){
+			start = as.Date(x[2], "%d.%m.%Y")
+			end = as.Date(x[3], "%d.%m.%Y")
+		}else{
+			start = as.Date(NA)
+			end = as.Date(x[4], "%d.%m.%Y")
+		}
+		fileInfos <- rbind(fileInfos, data.frame(id=id, start=start, end=end, filename=f))
 	}
-	fileInfos <- rbind(fileInfos, data.frame(id=id, start=start, end=end, filename=f))
-}
-# order files by end date
-fileInfos <- fileInfos[order(fileInfos$end),] 
+	# order files by end date
+	fileInfos <- fileInfos[order(fileInfos$end),] 
 
-# loop over all files and combine their data
-springData <- NULL
-for( j in 1:nrow(fileInfos)){
-	tmpFileCsv <- fileInfos$filename[j]
-	con <- file(tmpFileCsv,'r')
-	line <- '-'
-	while(line !='') line <- readLines(con,n=1)
+	# loop over all files and combine their data
+	springData <- NULL
+	for( j in 1:nrow(fileInfos)){
+		tmpFileCsv <- unzip(tmpFileArchive, as.character(fileInfos$filename[j]), junkpaths=TRUE, exdir=dirname(tmpFileArchive))
+		con <- file(tmpFileCsv,'r')
+		line <- '-'
+		while(line !='') line <- readLines(con,n=1)
 		
-	dat <- try(read.table(con, sep=';', header=TRUE, dec=','),silent=TRUE)
-	
-	close(con)
-	if (class(dat) == "try-error"){
-	  # remove the temporary csv file
-	  file.remove(tmpFileCsv) 
-	  next
-	}
-	# extract coordinates from file
-	if (class(dat) != "try-error"){
+		dat <- try(read.table(con, sep=';', header=TRUE, dec=','),silent=TRUE)
+		
+		close(con)
+		if (class(dat) == "try-error"){
+		  # remove the temporary csv file
+		  file.remove(tmpFileCsv) 
+		  next
+		}
+		# extract coordinates from file
+		if (class(dat) != "try-error"){
 		  
-	  coord_skip <- grep('Ostwert',readLines(tmpFileCsv))
+		  coord_skip <- grep('Ostwert',readLines(tmpFileCsv))
 		  
-	  if(coord_skip==0){
-	    WGS84 <- data.frame(lon_GK=NA,lat_GK=NA)
-		  
-	    }else{
-	    coord <- read.csv(tmpFileCsv, header=F, sep=';', skip=coord_skip-1, nrows=1, col.names=c('long_name','lon','lat_name','lat','projection'), colClasses=c('character','character','character'))
+		  if(coord_skip==0){
+		    WGS84 <- data.frame(lon_GK=NA,lat_GK=NA)
 		    
-	    # get coordinate in EPSG 31468 projection
-	    lon <- as.numeric(gsub('[^0-9.]','',coord$lon)); lat <- as.numeric(gsub('[^0-9.]','',coord$lat))
-	    GK <- data.frame(lon_GK=as.numeric(lon),lat_GK=as.numeric(lat))
+		  }else{
+		    coord <- read.csv(tmpFileCsv, header=F, sep=';', skip=coord_skip-1, nrows=1, col.names=c('long_name','lon','lat_name','lat','projection'), colClasses=c('character','character','character'))
 		    
-	    # add projection information (actually this converst the data.frame to a variable of class "SpatialPoints")
-	    coordinates(GK) <- c("lon_GK", "lat_GK")
-	    proj4string(GK) <- CRS("+proj=tmerc +lat_0=0 +lon_0=12 +k=1 +x_0=4500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs") # Defining Gauss-Krueger zone 4
+		    # get coordinate in EPSG 31468 projection
+		    lon <- as.numeric(gsub('[^0-9.]','',coord$lon)); lat <- as.numeric(gsub('[^0-9.]','',coord$lat))
+		    GK <- data.frame(lon_GK=as.numeric(lon),lat_GK=as.numeric(lat))
 		    
-	    # transform coordinates from Gauss-Kruger Zone 4 to WGS84
-	    WGS84 <- spTransform(GK, CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
-  	  }
+		    # add projection information (actually this converst the data.frame to a variable of class "SpatialPoints")
+		    coordinates(GK) <- c("lon_GK", "lat_GK")
+		    proj4string(GK) <- CRS("+proj=tmerc +lat_0=0 +lon_0=12 +k=1 +x_0=4500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs") # Defining Gauss-Krueger zone 4
+		    
+		    # transform coordinates from Gauss-Kruger Zone 4 to WGS84
+		    WGS84 <- spTransform(GK, CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+		  }
 		  
 		}
 		
-	names(dat) <- c('date','discharge','qualityState')
-	dat$date <- format(as.Date(dat$date),'%d.%m.%Y')
+		names(dat) <- c('date','discharge','qualityState')
+		dat$date <- format(as.Date(dat$date),'%d.%m.%Y')
 		
-	# convert discharge unit to m^3/s
-	dat$discharge <- dat$discharge*0.001
+		# convert discharge unit to m^3/s
+		dat$discharge <- dat$discharge*0.001
 		
-	springData <- rbind(springData, dat)
+		springData <- rbind(springData, dat)
 		
-  }
-	# file.remove(tmpFileArchive) # remove the temporary zip archive csv file
-
-for(i in 1:nrow(stationInfo)){		
+	}
+	file.remove(tmpFileArchive) # remove the temporary zip archive csv file
+	
 	cat(" -> write file")
-  selectedId <- stationInfo$Local_database_ID[i]
+	
 	metaData <- list(id = selectedId,
 	                 newID = stationInfo$Location.Identifier[i],
 	                 name = stationInfo$Name[i],
@@ -128,13 +174,10 @@ for(i in 1:nrow(stationInfo)){
 	
 	cat(" -> done :)\n")
 }
-
 # delete dir
 unlink(outfolder, recursive = T)
-# delete directory
-unlink("./WoKaS_Dynamic_Datasets/GKD Bavaria Data/", recursive = T)
-unlink("./dataset_GKD.zip")
 
-
+## -- end
+ 
 
 
